27488edf20accf3dbe43c3355968d964f8eb8cd1
Krisr190 <116085296+Krisr190@users.noreply.github.com>
2022-10-18 13:48:58 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,43 +1,43 @@
 /*eslint-env es6*/
-window.localQuotes = [
+ [
     {
-        text: "There are people who make no mistakes because they never wish to do anything worth doing",
-        author: "Goethe"
+        "text": "There are people who make no mistakes because they never wish to do anything worth doing",
+        "author": "Goethe"
     },
 	{
-	text: "There will come a time when you believe everything is finished. That will be the beginning",
-        author: "Louis L’Amour"
+	"text": "There will come a time when you believe everything is finished. That will be the beginning",
+        "author": "Louis L’Amour"
 	},
 	{
-	text: "The biggest problem we run into is going, ‘This is who I am, this is what I’m like, this is how I function’ while failing to notice that you don’t do that anymore",
-        author: "Neil Gaiman"
+	"text": "The biggest problem we run into is going, ‘This is who I am, this is what I’m like, this is how I function’ while failing to notice that you don’t do that anymore",
+        "author": "Neil Gaiman"
 	},
 	{
-	text: "Your visions will become clear only when you can look into your own heart. Who looks outside, dreams; who looks inside, awakens",
-        author: "C.G. Jung"
+	"text": "Your visions will become clear only when you can look into your own heart. Who looks outside, dreams; who looks inside, awakens",
+        "author": "C.G. Jung"
 	},
     {
-	text: "The truth is revealed by removing things which stand in its light, an art, not unlike sculpture, in which the artist creates, not by building, but by hacking away",
+	"text": "The truth is revealed by removing things which stand in its light, an art, not unlike sculpture, in which the artist creates, not by building, but by hacking away",
         author: "Alan Watts"
 	},
     {
-	text: "Don’t ever solve the same the same problem three times in a row. If you have the same question or problem three times in a row, systematize it",
-        author: "Joel Runyun"
+	"text": "Don’t ever solve the same the same problem three times in a row. If you have the same question or problem three times in a row, systematize it",
+        "author": "Joel Runyun"
 	},
     {
-	text: "Time is the king of all men, he is their parent and their grave, and gives them what he will and not what they crave",
-        author: "Pericles"
+	"text": "Time is the king of all men, he is their parent and their grave, and gives them what he will and not what they crave",
+        "author": "Pericles"
 	},
     {
-	text: "Nothing can be loved or hated unless it is first understood",
-        author: "Leonardo DaVinci"
+	"text": "Nothing can be loved or hated unless it is first understood",
+        "author": "Leonardo DaVinci"
 	},
     {
-	text: "Ability is what you are capable of, motivation determines what you do, but attitude determines how well you do it",
-        author: "Lou Holtz"
+	"text": "Ability is what you are capable of, motivation determines what you do, but attitude determines how well you do it",
+        "author": "Lou Holtz"
 	},
     {
-	text: "Wealth is liberty - liberty to seek recreation, liberty to enjoy life, liberty to improve the mind: it is disposable time and nothing more",
-        author: "Karl Marx"
+	"text": "Wealth is liberty - liberty to seek recreation, liberty to enjoy life, liberty to improve the mind: it is disposable time and nothing more",
+        "author": "Karl Marx"
 	},
 ]
