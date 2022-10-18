6e993a2cc03b06fd43f1a252e1064cdeff071b27
Krisr190 <116085296+Krisr190@users.noreply.github.com>
2022-10-18 14:12:51 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,27 +1,42 @@
 [
-{
-    
-    "text": "The greatest glory in living lies not in never falling, but in rising every time we fall",
-    "author": "Nelson Mandela"
-},
-  {
-    "text": "The way to get started is to quit talking and begin doing",
-    "author": "Walt Disney"
-},
     {
-    "text": "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking",
-    "author": "Steve Jobs"
-},
+        "text": "There are people who make no mistakes because they never wish to do anything worth doing",
+        "author": "Goethe"
+    },
+	{
+	"text": "There will come a time when you believe everything is finished. That will be the beginning",
+        "author": "Louis L’Amour"
+	},
+	{
+	"text": "The biggest problem we run into is going, ‘This is who I am, this is what I’m like, this is how I function’ while failing to notice that you don’t do that anymore",
+        "author": "Neil Gaiman"
+	},
+	{
+	"text": "Your visions will become clear only when you can look into your own heart. Who looks outside, dreams; who looks inside, awakens",
+        "author": "C.G. Jung"
+	},
     {
-    "text": "If life were predictable it would cease to be life, and be without flavor",
-    "author": "Eleanor Roosevelt"
-},
+	"text": "The truth is revealed by removing things which stand in its light, an art, not unlike sculpture, in which the artist creates, not by building, but by hacking away",
+        author: "Alan Watts"
+	},
     {
-    "text": "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success",
-    "author": "James Cameron"
-},
+	"text": "Don’t ever solve the same the same problem three times in a row. If you have the same question or problem three times in a row, systematize it",
+        "author": "Joel Runyun"
+	},
     {
-    "text": "Life is what happens when you're busy making other plans",
-    "author": "John Lennon"
-}
-    ]
+	"text": "Time is the king of all men, he is their parent and their grave, and gives them what he will and not what they crave",
+        "author": "Pericles"
+	},
+    {
+	"text": "Nothing can be loved or hated unless it is first understood",
+        "author": "Leonardo DaVinci"
+	},
+    {
+	"text": "Ability is what you are capable of, motivation determines what you do, but attitude determines how well you do it",
+        "author": "Lou Holtz"
+	},
+    {
+	"text": "Wealth is liberty - liberty to seek recreation, liberty to enjoy life, liberty to improve the mind: it is disposable time and nothing more",
+        "author": "Karl Marx"
+	}
+]
